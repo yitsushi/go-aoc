70aae2cf78b680dcaec72cb0deee1e08cfbcf7d1
Balazs Nadasdi <balazs@weave.works>
2021-11-16 17:35:17 +0100

    move testing from advent-of-code-2020

---
+++ testing/error.go
@@ -0,0 +1,13 @@
+package testing
+
+import "fmt"
+
+// FixtureFileNotFoundError occurs when the requested fixture file does not exist.
+type FixtureFileNotFoundError struct {
+	Path string
+	Name string
+}
+
+func (e FixtureFileNotFoundError) Error() string {
+	return fmt.Sprintf("Fixture file does not exist: %s/fixtures/%s", e.Path, e.Name)
+}
---
+++ testing/fixture_test.go
@@ -0,0 +1,63 @@
+package testing_test
+
+import (
+	"os"
+	"testing"
+
+	test "github.com/yitsushi/go-aoc/testing"
+)
+
+func TestLoadFixture_found(t *testing.T) {
+	data, err := test.LoadFixture("data")
+	if err != nil {
+		t.Errorf("TestLoadFixture_found should find the 'data' file")
+	}
+
+	expected := "yey\n"
+	if string(data) != expected {
+		t.Errorf("Expected file content: %s, got: %s", expected, data)
+	}
+}
+
+func TestLoadFixture_notFound(t *testing.T) {
+	data, err := test.LoadFixture("nodata")
+	if err == nil {
+		t.Errorf("TestLoadFixture_notFound should not find the 'nodata' file")
+	}
+
+	workingDirectory, _ := os.Getwd()
+
+	expectedError := test.FixtureFileNotFoundError{
+		Path: workingDirectory,
+		Name: "nodata",
+	}
+
+	if err.Error() != expectedError.Error() {
+		t.Errorf("expected error: '%s', got '%s'", expectedError.Error(), err.Error())
+	}
+
+	expected := ""
+	if string(data) != expected {
+		t.Errorf("Expected file content: %s, got: %s", expected, data)
+	}
+}
+
+func TestMust_noError(t *testing.T) {
+	input := []byte("Yey")
+	expected := "Yey"
+
+	output := test.Must(input, nil)
+	if string(output) != expected {
+		t.Errorf("Must() = %v, want %v", output, expected)
+	}
+}
+
+func TestMust_hasError(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("TestMust_hasError should have panicked!")
+		}
+	}()
+
+	_ = test.Must([]byte{}, test.FixtureFileNotFoundError{Path: "/path", Name: "file"})
+}
